4e65eb942af9db4e4021cf006d8dad8c497b323b
rubengalan <56016708+rubengalan@users.noreply.github.com>
2019-10-31 21:25:55 +0100

    Add files via upload

---
+++ js/halloween.js
@@ -0,0 +1,11 @@
+function mostrar(elemento){
+    document.getElementById(elemento).style.visibility="visible";
+}
+
+function ocultar(elemento){
+    document.getElementById(elemento).style.visibility="hidden";
+}
+
+function sonar(sonido){
+    document.getElementById(sonido).play();
+}
